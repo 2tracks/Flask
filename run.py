91e40e721ec8b5fe6589e986fd270362056b6928
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-04-05 19:36:39 +0000

    Deleted debug=True

--- run.py
+++ run.py
@@ -44,5 +44,4 @@ def careers():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
-    debug=True)
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")))
